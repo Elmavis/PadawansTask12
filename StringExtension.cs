8500eac94a00977a929a6a80c5d7554029368660
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-22 22:28:49 +0300

    1

--- StringExtension.cs
+++ StringExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PadawansTask12
 {
@@ -6,7 +7,19 @@ namespace PadawansTask12
     {
         public static bool AllCharactersAreUnique(string source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException();
+            if (source.Length == 0)
+                throw new ArgumentException();
+            List<char> l = new List<char>();
+            char[] arr = source.ToCharArray();
+            foreach (char c in arr)
+                if (l.Contains(c))
+                    return false;
+                else
+                    l.Add(c);
+
+            return true;
         }
     }
 }
